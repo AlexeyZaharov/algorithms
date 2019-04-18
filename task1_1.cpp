6f23ddbd17e62ae85017fa8eff3ed7c8a43f2907
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2019-04-19 01:57:02 +0300

    Update task1_1.cpp

--- task1_1.cpp
+++ task1_1.cpp
@@ -1,46 +1,184 @@
 #include <iostream>
+#include <string>
 
-void fill_array(int* array, const size_t& size) {
-    for (size_t i = 0; i < size; ++i) {
-        std::cin >> array[i];
+const size_t TABLE_INITIAL_SIZE= 8;
+const size_t a = 11;
+enum status {NIL, USING, DELETED} ;
+
+size_t hash(const std::string& str, const std::size_t& m) {
+    size_t hash = 0;
+    for( auto& ch : str ) {
+        hash = ( hash * a + ch ) % m;
     }
+    return hash;
 }
 
-std::pair<size_t, size_t> max_sum(const int* array_one, const int* array_two, const size_t& size) {
-    size_t right_index_one = 0, right_index_two = 0;
-    size_t real_index_one = 0;
+struct table_node {
+    std::string str;
+    int state;
 
-    for (size_t i = 1; i < size; ++i) {
-        if ( array_one[real_index_one] < array_one[i] ) {
-            real_index_one = i;
-        }
+    table_node() : str(""), state(status::NIL) {}
+};
 
-        if (array_one[real_index_one] + array_two[i] > array_one[right_index_one] + array_two[right_index_two]) {
-            right_index_one = real_index_one;
-            right_index_two = i;
+class hash_table {
+private:
+    size_t _count;
+    size_t _capacity;
+    table_node* _table;
+
+    void rehash();
+
+public:
+    hash_table() : _count(0), _capacity(TABLE_INITIAL_SIZE) {
+        _table = new table_node[_capacity];
+    }
+
+    bool insert(const std::string& str);
+    bool remove(const std::string& str);
+    bool find(const std::string& str);
+
+    void out() {
+        for (size_t i = 0; i < _capacity; ++i) {
+            std::cout << i << "\t\t" << _table[i].str << "\t\t" << _table[i].state << std::endl;
         }
     }
 
-    return std::make_pair(right_index_one, right_index_two);
-}
+    ~hash_table() {
+        delete[] _table;
+    }
+};
+
 
 int main() {
-    size_t size = 0;
+    hash_table table;
+    while( true ) {
+        char command = 0;
+        std::string value;
+        std::cin >> command >> value;
 
-    std::cin >> size;
+        if( std::cin.fail() )
+        {
+            break;
+        }
 
-    int* array_one = new int[size];
-    int* array_two = new int[size];
+        switch( command )
+        {
+            case '?':
+                std::cout << ( table.find( value ) ? "OK" : "FAIL" ) << std::endl;
+                break;
+            case '+':
+                std::cout << ( table.insert( value ) ? "OK" : "FAIL" ) << std::endl;
+                break;
+            case '-':
+                std::cout << ( table.remove( value ) ? "OK" : "FAIL" ) << std::endl;
+                break;
+        }
 
-    fill_array(array_one, size);
-    fill_array(array_two, size);
+        table.out();
 
-    auto pair = max_sum(array_one, array_two, size);
-
-    std::cout << pair.first << ' ' << pair.second;
-
-    delete[] array_one;
-    delete[] array_two;
+    }
 
     return 0;
+
+}
+
+void hash_table::rehash() {
+    size_t new_capacity = _capacity*2;
+
+    table_node* new_table = new table_node[new_capacity];
+
+    for (size_t i = 0; i < _capacity; ++i) {
+        if (_table[i].state == status::USING) {
+
+            size_t h = hash(_table[i].str, new_capacity);
+            size_t j = 0;
+
+            while( j < new_capacity ) {
+                if (new_table[h].state != status::USING) {
+                    break;
+                }
+
+                h += j + 1;
+                h %= new_capacity;
+
+                ++j;
+            }
+
+            new_table[h].str = _table[i].str;
+            new_table[h].state = _table[i].state;
+        }
+    }
+
+    delete[] _table;
+    _capacity = new_capacity;
+    _table = new_table;
+}
+
+bool hash_table::insert(const std::string& str) {
+
+    bool success = false;
+    size_t h = hash(str, _capacity);
+
+    for(size_t i = 0; i < _capacity; ++i) {
+
+        if (_table[h].str == str && _table[h].state == status::USING) {
+            break;
+        }
+
+        if (_table[h].state == status::USING) {
+                h += i + 1;
+                h %= _capacity;
+            }
+        else {
+            _table[h].str = str;
+            _table[h].state = status::USING;
+
+            ++_count;
+            success = true;
+            break;
+        }
+    }
+
+    if (double(_count)/double(_capacity) >= 0.75 ) {
+        rehash();
+    }
+
+    return success;
+}
+
+bool hash_table::remove(const std::string &str){
+    bool success = false;
+    size_t h = hash(str, _capacity);
+
+    for(size_t i = 0; i < _capacity; ++i) {
+        if (_table[h].str == str && _table[h].state == status::USING) {
+            _table[h].state = status::DELETED;
+            --_count;
+            success = true;
+            break;
+        }
+        else if (_table[h].state != status::NIL) {
+            h += i + 1;
+            h %= _capacity;
+        }
+        else {
+            break;
+        }
+    }
+
+    return success;
+}
+
+bool hash_table::find( const std::string &str ) {
+    size_t i = 0;
+    size_t h = hash(str, _capacity);
+    while( _table[h].state != status::NIL && i < _capacity ) {
+        if( _table[h].str == str && _table[h].state == status::USING){
+            return true;
+        }
+        h += i + 1;
+        h %= _capacity;
+        ++i;
+    }
+    return false;
 }
